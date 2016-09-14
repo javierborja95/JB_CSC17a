c0f7882cc6bb3f8ba5a39cdc13cc7eea321499f1
javierborja95 <javierborja95@gmail.com>
2016-09-14 12:06:02 -0700

    hw

--- Assignments/Assignment_02/Gaddis_8thEd_Chap07_Prob06_RainOrShine/main.cpp
+++ Assignments/Assignment_02/Gaddis_8thEd_Chap07_Prob06_RainOrShine/main.cpp
@@ -1,30 +1,103 @@
 /* 
  * File:   main.cpp
  * Author: Javier Borja
- * Created on September , 2016,  PM
- * Purpose:
+ * Created on September 13, 2016, 1:30 PM
+ * Purpose: Displays how many days were a particular weather
  */
 
 //System Libraries
 #include <iostream>   //Input/ Output Stream Library
+#include <fstream>    //File I/O
+#include <cstdlib>    //Random
+#include <ctime>      //Time
 using namespace std;  //Namespace of the System Libraries
 
 //User Libraries
 
 //Global Constants
-
+const int DAY=30; //Number of days in month
 //Function Prototypes
-
+void writAry(int);
+void inAry(char[][DAY],int);
+char getWthr();
+void display(char[][DAY],int[],int);
 //Execution
 
 int main(int argc, char** argv) {
+    //Set Random Seed
+    srand(static_cast<unsigned int>(time(0)));
     //Variables
-
+    const int MONTH=3; //Months: June, July, August
+    char weather[MONTH][DAY];
+    int count[MONTH]={0,0,0};
+    int result[MONTH];
     //Input Data
-
+    writAry(MONTH);
+    inAry(weather,MONTH);
     //Process Data
     
     //Output Data
-
+    display(weather,count,MONTH);
     return 0;
+}
+
+void writAry(int n){
+    //Variables
+    ofstream write;
+    char temp;
+    //Open Files
+    write.open("weather.dat");
+    //Process Data and Output Data
+    for(int i=0;i<n;i++){
+        for(int j=0;j<DAY;j++){
+            temp=getWthr();
+            write<<temp<<" ";
+        }
+        write<<"\r";
+    }
+    write.close();
+}
+
+char getWthr(){
+    //Variables
+    const int OPTION=3;
+    char weather[OPTION]={'R','C','S'}; //Rainy, cloudy, sunny
+    //Output Data
+    return(weather[rand()%3]); //0,1,2
+}
+
+void inAry(char a[][DAY],int n){
+    //Variables
+    ifstream read;
+    //Open Files and Input Data
+    read.open("weather.dat");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<DAY;j++){
+            read>>a[i][j];
+        }
+    }
+    read.close();
+}
+
+void display(char a[][DAY],int b[],int n){
+    //Output Data
+    for(int i=0;i<n;i++){
+        switch(i){
+            case 0:cout <<"June ";break;
+            case 1:cout <<"July ";break;
+            default:cout<<" Aug ";break;
+        }
+        for(int j=0;j<DAY;j++){
+            cout<<a[i][j]<<" ";
+            if(a[i][j]=='R')      //Rainy
+                b[0]++;
+            else if(a[i][j]=='C') //Cloudy
+                b[1]++;
+            else b[2]++;          //'S' sunny
+        }
+        cout<<endl;
+    }
+    cout<<b[0]<<" days were rainy."<<endl;
+    cout<<b[1]<<" days were cloudy."<<endl;
+    cout<<b[2]<<" days were sunny."<<endl;
 }
---
+++ Assignments/Assignment_02/Gaddis_8thEd_Chap08_Prob07_BinaryStringSearch/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on September 14, 2016, 11:00 AM
+ * Purpose: Sorts and searches through names
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+using namespace std;  //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+    //Variables
+const int NAMES = 20;
+string names[NAMES] = {"Collins, Bill", "Smith, Bart", "Allen, Jim",
+                           "Griffin, Jim", "Stamey, Marty", "Rose, Geri",
+                           "Taylor, Terri", "Johnson, Jill",
+                           "Allison, Jeff", "Looney, Joe", "Wolfe, Bill",
+                           "James, Jean", "Weaver, Jim", "Pore, Bob",
+                           "Rutherford, Greg", "Javens, Renee",
+                           "Harrison, Rose", "Setzer, Cathy",
+                           "Pike, Gordon", "Holland, Beth" };
+    //Input Data
+
+    //Process Data
+    
+    //Output Data
+
+    return 0;
+}
